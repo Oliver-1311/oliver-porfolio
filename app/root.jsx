802a14d041b2fce5d84375f15587eccadf103176
Oliver Macedo <oli932300102@gmail.com>
2024-03-28 13:53:43 -0500

    set up project(dependences and plugins)

--- app/root.jsx
+++ app/root.jsx
@@ -1,12 +1,6 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function Layout({ children }) {
   return (
     <html lang="en">
       <head>
